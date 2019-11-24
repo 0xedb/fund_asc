526899265183186dc9bc9fec0c4942a62aacc25a
theBashShell <edohbruno@gmail.com>
2019-11-24 11:24:21 +0000

    progress

---
+++ src/components/HeadMeta.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Helmet} from 'react-helmet';
+
+function HeadMeta() {
+    return (
+        <Helmet>
+            <title>Fund ASC</title>
+            <link rel='icon' href='../images/favicon.png' type='image/png' />
+        </Helmet>
+    )
+}
+
+export default HeadMeta
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeadMeta from '../components/HeadMeta';
+
+function index() {
+    return (
+        <>
+        <HeadMeta />
+        <div>
+            hello
+        </div>
+        </>
+    )
+}
+
+export default index
